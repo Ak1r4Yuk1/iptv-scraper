f4b7da442af7a6b2f4ea8440e3ac718ce995e63b
Akira <niko.maiorana@gmail.com>
2020-10-02 11:01:55 +0200

    Version 1.0

---
+++ iptv-scraper/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(45, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Italy"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(45, 73)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Germany"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(126, 73)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Spain"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(126, 26)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "UK"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Teal
+        Me.ClientSize = New System.Drawing.Size(238, 129)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "Download"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+End Class
---
+++ iptv-scraper/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Web
+
+Public Class Form1
+    Dim desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myDt As DateTime = DateTime.Now
+
+        myDt.ToString("ddMMyy")
+
+        Dim filename = "https://www.iptvsource.com/lists/it_" & myDt.ToString("ddMMyy") & "_iptvsource_com3.m3u"
+        Dim filename1 = "https://www.iptvsource.com/lists/it_" & myDt.ToString("ddMMyy") & "_iptvsource_com4.m3u"
+        Dim filename2 = "https://www.iptvsource.com/lists/it_" & myDt.ToString("ddMMyy") & "_iptvsource_com5.m3u"
+
+        MsgBox("Check Your iptv Folder in Desktop for File italy.m3u")
+        Try
+
+            My.Computer.Network.DownloadFile(filename, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-italy.m3u")
+            My.Computer.Network.DownloadFile(filename1, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-italy2.m3u")
+            My.Computer.Network.DownloadFile(filename2, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-italy3.m3u")
+
+        Catch
+            MsgBox("If some lists are missing try again next time..")
+        End Try
+
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim myDt As DateTime = DateTime.Now
+
+        myDt.ToString("ddMMyy")
+        Dim filename = "https://www.iptvsource.com/lists/uk_" & myDt.ToString("ddMMyy") & "_iptvsource_com3.m3u"
+        Dim filename1 = "https://www.iptvsource.com/lists/uk_" & myDt.ToString("ddMMyy") & "_iptvsource_com4.m3u"
+        Dim filename2 = "https://www.iptvsource.com/lists/uk_" & myDt.ToString("ddMMyy") & "_iptvsource_com5.m3u"
+
+        MsgBox("Check Your iptv Folder in Desktop for File uk.m3u")
+
+        Try
+            My.Computer.Network.DownloadFile(filename, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-uk.m3u")
+            My.Computer.Network.DownloadFile(filename1, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-uk2.m3u")
+            My.Computer.Network.DownloadFile(filename2, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-uk3.m3u")
+
+        Catch
+            MsgBox("If some lists are missing try again next time..")
+
+        End Try
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myDt As DateTime = DateTime.Now
+
+        myDt.ToString("ddMMyy")
+        Dim filename = "https://www.iptvsource.com/lists/de_" & myDt.ToString("ddMMyy") & "_iptvsource_com3.m3u"
+        Dim filename1 = "https://www.iptvsource.com/lists/de_" & myDt.ToString("ddMMyy") & "_iptvsource_com4.m3u"
+        Dim filename2 = "https://www.iptvsource.com/lists/de_" & myDt.ToString("ddMMyy") & "_iptvsource_com5.m3u"
+
+        MsgBox("Check Your iptv Folder in Desktop for File germany.m3u")
+        Try
+            My.Computer.Network.DownloadFile(filename, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-germany.m3u")
+            My.Computer.Network.DownloadFile(filename1, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-germany2.m3u")
+            My.Computer.Network.DownloadFile(filename2, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-germany3.m3u")
+        Catch
+            MsgBox("If some lists are missing try again next time..")
+        End Try
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim myDt As DateTime = DateTime.Now
+
+        myDt.ToString("ddMMyy")
+        Dim filename = "https://www.iptvsource.com/lists/es_" & myDt.ToString("ddMMyy") & "_iptvsource_com3.m3u"
+        Dim filename1 = "https://www.iptvsource.com/lists/es_" & myDt.ToString("ddMMyy") & "_iptvsource_com4.m3u"
+        Dim filename2 = "https://www.iptvsource.com/lists/es_" & myDt.ToString("ddMMyy") & "_iptvsource_com5.m3u"
+
+        MsgBox("Check Your iptv Folder in Desktop for File spain.m3u")
+        Try
+            My.Computer.Network.DownloadFile(filename, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-spain.m3u")
+            My.Computer.Network.DownloadFile(filename1, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-spain2.m3u")
+            My.Computer.Network.DownloadFile(filename2, desktop + "\iptv\" & myDt.ToString("dd-MM-yy") & "-spain3.m3u")
+        Catch
+            MsgBox("If some lists are missing try again next time..")
+        End Try
+
+
+    End Sub
+End Class
